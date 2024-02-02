da8c44bbd8ed45478782fea15c9e4c98255242fc
Thomas Groenewegen <t.n.groenewegen@student.vu.nl>
2024-02-02 14:03:00 +0100

    turned this into a function

--- scripts/caption_to_embedding.py
+++ scripts/caption_to_embedding.py
@@ -7,31 +7,18 @@ import numpy as np
 import torch
 import string
 
-# Load pre-trained word2vec model
-model = api.load("glove-wiki-gigaword-100")
-
-
-
-def transform_captions(captions):
-    embeddings = []
-    for caption in captions:
-        # Remove punctuation and convert to lowercase
-        caption = caption.translate(str.maketrans('', '', string.punctuation)).lower()
-        words = caption.split()
-        caption_embedding = np.zeros(model.vector_size)
-        count = 0
-        for word in words:
-            if word in model.key_to_index:
-                caption_embedding += model[word]
-                count += 1
-            else:
-                print(f'Word not in vocabulary: {word}')  # Add this line
-                
-        if count > 0:
-            caption_embedding /= count
-        embeddings.append(caption_embedding)
-    return embeddings
-# Example usage
-captions = ["This is a caption.", "Another caption."]
-caption_embeddings = transform_captions(captions)
-print(caption_embeddings)
+def transform_captions(caption, model):
+    caption = caption.translate(str.maketrans('', '', string.punctuation)).lower()
+    words = caption.split()
+    caption_embedding = np.zeros(model.vector_size)
+    count = 0
+    for word in words:
+        if word in model.key_to_index:
+            caption_embedding += model[word]
+            count += 1
+        else:
+            print(f'Word not in vocabulary: {word}')  # Add this line
+            
+    if count > 0:
+        caption_embedding /= count
+    return caption_embedding  # Return the array directly
